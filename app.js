c39441eca52d99d8ffc59f9babc7d8419fc8c1f4
anonymousdomain <domainanonymous@gmail.com>
2021-11-03 23:22:44 +0300

    adding booklist from input from is implimented

--- app.js
+++ app.js
@@ -50,5 +50,18 @@ class UI {
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
 
 //event:add a book
+document.querySelector('#book-form').addEventListener('submit',
+(e)=>{
+    e.preventDefault();
+
+    const title=document.querySelector('#title').value;
+    const author=document.querySelector('#author').value;
+    const isbn=document.querySelector('#isbn').value;
+    
+    const book=new Book(title,author,isbn);
+
+    // add book to UI
+    UI.addBookToList(book);
+})
 
 // event remove a book
