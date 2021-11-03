c959a4ae2b99483e3a70ce5e86f806e137957235
anonymousdomain <domainanonymous@gmail.com>
2021-11-04 00:24:02 +0300

    set time out to distroy the validation message

--- app.js
+++ app.js
@@ -59,6 +59,10 @@ class UI {
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
+
+        // make the validation disappear in 2 sec
+
+        setTimeout(()=>document.querySelector('.alert').remove(),2000);
     }
 }
 
